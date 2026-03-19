391b784787c6fdbf090057955feb1b1189051eea
drewhk <drewhk@drewhk.com>
2026-03-19 10:19:33 +0100

    More debug statement

--- src/nordberg.rs
+++ src/nordberg.rs
@@ -68,8 +68,11 @@ pub fn solve(world_3d_points: &[[f64; 3]; 3], bearing_vectors: &[[f64; 3]; 3]) -
     compute_poses_nordberg(world_3d_points, bearing_vectors)
         .into_iter()
         .map(|(rot, trans)| {
+            println!("MAT1");
             let rotation = UnitQuaternion::from_matrix(&rot);
+            println!("MAT2");
             let translation = Translation::from(trans);
+            println!("MAT3");
             Pose::from_iso3(Iso3::from_parts(translation, rotation))
         })
         .collect()
